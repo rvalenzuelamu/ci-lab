0d356f622518c139b0885409cc8a14b5d8ea8169
Rodolfo Collio <collio95@gmail.com>
2022-10-19 11:50:36 -0300

    Corrijo respuesta

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -22,7 +22,7 @@ public class App {
     }
     
     if((a*a + b*b) == c*c){
-      return "es un triangulo escaleno";
+      return "es un triangulo pitagorico";
     }
       return "error no es un triangulo";
   }
