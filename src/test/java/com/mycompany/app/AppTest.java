9d3c591f947c91002ff437b9676794f1362ff327
robertconeme <robert.valenzuela08@gmail.com>
2023-10-30 20:55:16 -0300

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -13,7 +13,7 @@ public class AppTest {
   public void probarTriangulo() {
     assertEquals("error no es un triangulo", App.triangulo(0, 0, 0));
     assertEquals("es un triangulo equilatero", App.triangulo(1, 1, 1));
-    //assertEquals("es un triangulo isoceles", App.triangulo(2, 2, 3));
+    assertEquals("es un triangulo isoceles", App.triangulo(2, 2, 3));
     // assertEquals("es un triangulo escaleno", App.triangulo(3, 4, 6));
     // assertEquals("es un triangulo pitagorico", App.triangulo(3, 4, 5));
   }
